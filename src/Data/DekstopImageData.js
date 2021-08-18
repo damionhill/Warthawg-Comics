15a7c63fa33941bb431e5f37abff4878db586e2b
Alanna <aehill@umich.edu>
2021-08-18 17:23:13 -0400

    Editing instructions and separating image data for mobile carousel from desktop displayed images

---
+++ src/Data/DekstopImageData.js
@@ -0,0 +1,31 @@
+import image1 from "../Gallery Images/Gallery1.jpg"
+import image2 from "../Gallery Images/Gallery2.jpg"
+import image3 from "../Gallery Images/Gallery3.jpg"
+import image4 from "../Gallery Images/Gallery4.jpg"
+import image5 from "../Gallery Images/Gallery5.jpg"
+import image6 from "../Gallery Images/Gallery6.jpg"
+
+export const DesktopImageData = [
+    {
+        image: image1
+
+    },
+    {
+        image: image2
+
+    },
+    {
+        image: image3
+
+    },
+    {
+        image: image4
+    },
+    {
+        image: image5
+    },
+    {
+        image: image6
+    }
+
+]
