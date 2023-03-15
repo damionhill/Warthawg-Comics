ceba5d866cd3ff78a85e88a748b3f82a29f9c1a0
Damion Hill <chilihill2@gmail.com>
2023-03-15 09:40:48 -0400

    events

--- src/views/Events.js
+++ src/views/Events.js
@@ -17,8 +17,8 @@ function Events()
 
         {/*DAD! Add events below this line by copying the format. */}
         {/* <EventInstance linkURL = "http://www.shoffpromotions.com/" name = "Comic Logic Comic Show" date = "Jul 11, 2021 12-5PM" address = "44031 Ashburn Plaza Shopping Center #281 Ashburn, VA 20147"/> */}
-            <EventInstance linkURL = "http://www.clandestinecomics.com/" name = "Laurel, MD Comic Book Show" date = "Feb 12, 2023 10-3PM" address = "15101 Sweltzer Lane Laurel, MD Double Tree Hotel"/>
-            <EventInstance linkURL = "http://www.shoffpromotions.com/" name = "Annandale, VA Comic Book Show" date = "Feb 26, 2023 10-3PM" address = "7128 Columbia Pike Annandale, VA 22003"/>
+            <EventInstance linkURL = "http://www.clandestinecomics.com/" name = "Laurel, MD Comic Book Show" date = "Mar 19, 2023 10-3PM" address = "5400 Holiday Drive Frederick, MD Clarion Inn"/>
+            <EventInstance linkURL = "http://www.shoffpromotions.com/" name = "Annandale, VA Comic Book Show" date = "Mar, 2023 10-3PM" address = "7128 Columbia Pike Annandale, VA 22003"/>
           
 
 
