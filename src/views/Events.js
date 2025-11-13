0c9999663ba59633fbd8dc79c0495e398c2222ce
Damion Hill <chilihill2@gmail.com>
2025-11-12 21:32:20 -0500

    update events

--- src/views/Events.js
+++ src/views/Events.js
@@ -18,9 +18,9 @@ function Events()
         {/*DAD! Add events below this line by copying the format. */}
         {/* <EventInstance linkURL = "http://www.shoffpromotions.com/" name = "Comic Logic Comic Show" date = "Jul 11, 2021 12-5PM" address = "44031 Ashburn Plaza Shopping Center #281 Ashburn, VA 20147"/> */}
         
-        <EventInstance linkURL = "http://www.warthawgcomics.com/" name = "Comic Logic Lot Con" date = "Sep 7,2025 10-3PM" address = "Comic Logic 44031 Ashburn Shopping Plz Ste 281 Ashburn, VA 20147"/>
         <EventInstance linkURL = "http://www.shoffpromotions.com/" name = "Frederick, MD, Comic Book Show" date = "Nov 16,2025 10-3PM" address = "5400 Holiday Drive Frederick, MD 21703"/>
-        <EventInstance linkURL = "http://www.baltimorecomicconvention.com" name = "Baltimore Comic Convention" date = "Oct 17-19,2025" address = "Baltimore Convention Center Booth 904"/>
+        <EventInstance linkURL = "http://www.lehighvalleycomicconvention.com" name = "Lehigh Valley Comic Con " date = "Dec 6,2025" address = "LCCC 4525 Education Park Dr Schnecksville, PA"/>
+        <EventInstance linkURL = "http://www.clandestinecomics.com" name = "Laurel MD Comic Show " date = "Dec 7,2025" address = "Double Tree Hotel 15101 Sweitzer Lane Laurel, MD"/>
   </div>
 
     </>
